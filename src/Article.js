b4a948a8180abdaf5f158006e65a19cd31d3805f
dhancode46 <wirawardhana49@gmail.com>
2022-01-19 05:13:22 +0700

    added date lang id

--- src/Article.js
+++ src/Article.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import moment from 'moment';
+import 'moment/locale/id';
+// moment.locale('id');
 
 function Article({ img, id, title, category, berita, menit, date }) {
-  console.log(date);
   return (
     <article className='news-card' key={id}>
       <img src={img} alt='' />
       <div className='news-info'>
         <h4 className='news_category'>{category}</h4>
-        <p className='new_date'>{moment(date).format('Do MMMM YYYY')}</p>
+        <p className='new_date'>{moment(date).format('DD MMMM YYYY')}</p>
         <h3 className='news_title'>{title}</h3>
         <p className='news_time'>
           <span>{berita}</span> - {menit} menit yang lalu
